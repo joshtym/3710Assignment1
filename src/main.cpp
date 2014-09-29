d89365b89fc6f2740cee927f462730a8228a8ea3
Josh T <josh.tymburski@uleth.ca>
2014-09-29 00:32:47 -0600

    Finish creation of mockup. Horribly inefficient. Will fix

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ void keyboard(unsigned char key, int xPos, int yPos);
 void mouse(int button, int state, int xPos, int yPos);
 void reshape(int width, int height);
 
+static GLfloat spin = 0.0;
+
 int main(int argc, char **argv)
 {
 	/* Supplied -- Do not change! */
@@ -33,22 +35,169 @@ int main(int argc, char **argv)
 // Function Definitions
 void init()
 {
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_SMOOTH);
 }
 
 void display()
 {
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	glPushMatrix();
+	glRotatef(spin, 1.0, 0.0, 0.0);
+	
+	glBegin(GL_LINE_LOOP);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.00, 0.00, 0.00);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.00, 0.00, -1.00);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.50, 0.00, -1.00);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.75,0.00, -0.75);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.00, 0.00, 0.00);
+		
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.50, 0.50, -0.50);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.25, 0.25, -1.00);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.00, 0.00, -1.00);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.00, 0.00, 0.00);
+		
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.75, 0.00, -0.75);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.50, 0.50, -0.50);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.00, 0.00, 0.00);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.75, 0.00, -0.75);
+		
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.50, 0.00, -1.00);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.25, 0.25, -1.00);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.50, 0.50, -0.50);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.75, 0.00, -0.75);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.50, 0.00, -1.00);
+		
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.00, 0.00, -1.00);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.25, 0.25, -1.00);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(0.50, 0.00, -1.00);
+		
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.00, 0.00, 0.00);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.00, 0.00, 1.00);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.50, 0.00, 1.00);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.75,0.00, 0.75);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.00, 0.00, 0.00);
+		
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.50, 0.50, 0.50);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.25, 0.25, 1.00);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.00, 0.00, 1.00);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.00, 0.00, 0.00);
+		
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.75, 0.00, 0.75);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.50, 0.50, 0.50);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.00, 0.00, 0.00);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.75, 0.00, 0.75);
+		
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.50, 0.00, 1.00);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.25, 0.25, 1.00);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.50, 0.50, 0.50);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.75, 0.00, 0.75);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.50, 0.00, 1.00);
+		
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.00, 0.00, 1.00);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.25, 0.25, 1.00);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.50, 0.00, 1.00);
+	glEnd();
+	
+	glPopMatrix();
+	glutSwapBuffers();
+	glFlush();
 }
 
 void action()
 {
+	spin = spin + 0.02;
+	
+	if (spin > 360.0)
+		spin = spin - 360;
+	
+	glutPostRedisplay();
 }
 
 void keyboard(unsigned char key, int xPos, int yPos)
 {
+	switch(key)
+	{
+		case 27:
+			break;
+		case 82:
+			break;
+		case 114:
+			break;
+		case 88:
+			glutIdleFunc(action);
+			break;
+		case 120:
+			glutIdleFunc(action);
+			break;
+		case 89:
+			break;
+		case 121:
+			break;
+		case 90:
+			break;
+		case 122:
+			break;
+		default:
+			break;
+	}
+			
 }
 
 void mouse(int button, int state, int xPos, int yPos)
 {
+	switch(button)
+	{
+		case GLUT_LEFT_BUTTON:
+			break;
+		case GLUT_RIGHT_BUTTON:
+			break;
+		default:
+			break;
+	}
 }
 
 void reshape(int width, int height)
